28c471d5d3555ae0e21ff423b4c671229112e6d6
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-07-01 17:32:54 +0200

    Set up Express server and integrate posts routes

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+const postRoutes = require('./routes/posts'); // 👈 importiamo le rotte
+
+app.use('/posts', postRoutes); // 👈 assegnate alla rotta base /posts
+
+app.listen(port, () => {
+    console.log(`Server avviato su http://localhost:${port}`);
+});
