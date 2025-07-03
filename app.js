d75f227ff72e02e650f3cdd748d51360a4cc506e
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-07-03 11:45:57 +0200

    Add static file serving middleware for public assets

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000
 app.use(express.json())
+app.use(express.static('public'));
 
 const postRoutes = require('./routes/posts'); //  importiamo le rotte
 
